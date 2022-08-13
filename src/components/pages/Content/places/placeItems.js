0afb4bc914b915da508aba1d7d528365ea07cdd6
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-13 06:33:50 -0700

    add images on place details collections

--- src/components/pages/Content/places/placeItems.js
+++ src/components/pages/Content/places/placeItems.js
@@ -4,7 +4,7 @@ const Placeitems = [
     name: "Thika Falls",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    imgSrc: "",
+    imgSrc: "https://www.stockvault.net/data/2015/03/29/169788/preview16.jpg",
     alt: "thika fall"
   },
   {
@@ -12,7 +12,7 @@ const Placeitems = [
     name: "Ngare Dare",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    imgSrc: "/placeimages/Tour_logo.jpg",
+    imgSrc: "https://www.stockvault.net/data/2017/05/29/237064/preview16.jpg",
     alt: "ngare dare"
   },
   {
@@ -20,7 +20,7 @@ const Placeitems = [
     name: "Mt. Kenya",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    imgSrc: "/placeimages/Tour_logo.jpg",
+    imgSrc: "https://www.stockvault.net/data/2019/03/09/261913/preview16.jpg",
     alt: "mt kenya"
   },
   {
@@ -28,7 +28,7 @@ const Placeitems = [
     name: "Naivasha",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    imgSrc: "/placeimages/Tour_logo.jpg",
+    imgSrc: "https://www.stockvault.net/data/2014/01/07/152528/preview16.jpg",
     alt: "naivasha"
   }
 ];
