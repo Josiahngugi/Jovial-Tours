5cac3356eaec89ef5201133cb187cc51a89775bb
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-13 01:13:32 -0700

    link pages to routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Africa from "./components/pages/Africa";
+import America from "./components/pages/America";
+import Asia from "./components/pages/Asia";
+import Europe from "./components/pages/Europe";
 
 import App from "./App";
 
@@ -11,10 +15,10 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      {/* <Route path="africa" element={<Africa />} />
+      <Route path="africa" element={<Africa />} />
       <Route path="america" element={<America />} />
       <Route path="europe" element={<Europe />} />
-      <Route path="asia" element={<Asia />} /> */}
+      <Route path="asia" element={<Asia />} />
     </Routes>
   </BrowserRouter>
 );
