6bd01cf0f7059198a89dbd5e1b888073207822a7
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-12 22:44:37 -0700

    create menu folder Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import Menu from "./components/menu/Menu";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello TOur</h1>
+    <div>
+      <Menu />
     </div>
   );
 }
