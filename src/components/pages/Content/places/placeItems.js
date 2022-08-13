17f006abd482cf85b93bf74d3eecd8d8226110e4
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-13 05:49:06 -0700

    add places' data

---
+++ src/components/pages/Content/places/placeItems.js
@@ -0,0 +1,36 @@
+const Placeitems = [
+  {
+    id: 0,
+    name: "Thika Falls",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    imgSrc: "",
+    alt: "thika fall"
+  },
+  {
+    id: 1,
+    name: "Ngare Dare",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    imgSrc: "/placeimages/Tour_logo.jpg",
+    alt: "ngare dare"
+  },
+  {
+    id: 2,
+    name: "Mt. Kenya",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    imgSrc: "/placeimages/Tour_logo.jpg",
+    alt: "mt kenya"
+  },
+  {
+    id: 3,
+    name: "Naivasha",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    imgSrc: "/placeimages/Tour_logo.jpg",
+    alt: "naivasha"
+  }
+];
+
+export default Placeitems;
