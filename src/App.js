5ce93c939bf29e4eaf142cbf21557b80941750a1
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-13 00:38:16 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Menu from "./components/menu/Menu";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <Menu />
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 
@@ -7,7 +8,13 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* <Route path="africa" element={<Africa />} />
+      <Route path="america" element={<America />} />
+      <Route path="europe" element={<Europe />} />
+      <Route path="asia" element={<Asia />} /> */}
+    </Routes>
+  </BrowserRouter>
 );
