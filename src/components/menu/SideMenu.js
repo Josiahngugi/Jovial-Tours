0b9998bb03bd3de41843f4b005b977c0761f2760
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-12 23:19:55 -0700

    add menu items and icons

--- src/components/menu/SideMenu.js
+++ src/components/menu/SideMenu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MenuItems from "./MenuItems";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -32,26 +33,11 @@ function SideMenu(props) {
       <Toolbar />
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {MenuItems.map((menu) => (
+          <ListItem key={menu.id} disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemIcon>{menu.icon}</ListItemIcon>
+              <ListItemText primary={menu.label} />
             </ListItemButton>
           </ListItem>
         ))}
