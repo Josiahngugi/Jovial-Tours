993485697e33aa939fd09d934ed4d99fcf2854a7
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2022-08-12 23:16:04 -0700

    add menu list file route, icon, label

---
+++ src/components/menu/MenuItems.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import AnimationIcon from "@mui/icons-material/Animation";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import AppRegistrationIcon from "@mui/icons-material/AppRegistration";
+const MenuItems = [
+  {
+    id: 0,
+    label: "Africa",
+    icon: <AcUnitIcon />,
+    route: "africa"
+  },
+  {
+    id: 1,
+    label: "America",
+    icon: <AnimationIcon />,
+    route: "america"
+  },
+  {
+    id: 2,
+    label: "Europe",
+    icon: <ApartmentIcon />,
+    route: "europe"
+  },
+  {
+    id: 3,
+    label: "Asian",
+    icon: <AppRegistrationIcon />,
+    route: "asian"
+  }
+];
+
+export default MenuItems;
